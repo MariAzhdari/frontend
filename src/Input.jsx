c7d1848257f15a5ed107b6e395d941f59561e792
Mari Azh <Marzi.aghchay@gmail.com>
2023-06-12 23:57:23 +0100

    start

---
+++ src/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div>
+      <input type="text" placeholder="inter url" />
+      <input type="text" placeholder="inter url" />
+    </div>
+  );
+}
+
+export default Input
+
