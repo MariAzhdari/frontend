8f816259bcd125294753fa73629a60f56d9698ea
Mari Azh <Marzi.aghchay@gmail.com>
2023-06-15 19:21:22 +0100

    .

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Input() {
+  const [book, setBook] = useState({
+    url: "",
+    title: ""
+  });
+  const [error, setError] = useState(false);
+
+  // const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/new_table", book);
+
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+  };
+
   return (
     <div>
-      <input type="text" placeholder="inter url" />
-      <input type="text" placeholder="inter url" />
+      <input type="text" placeholder="inter url" onChange={handleChange} />
+      <input type="text" placeholder="inter title" />
+      <button className="add" onChange={handleChange}>
+        add
+      </button>
     </div>
   );
 }
 
-export default Input
-
+export default Input;
